1b577be8fcfe6c31a4dc6088170980991a9f4eff
thelogeshwaran <47322586+thelogeshwaran@users.noreply.github.com>
2020-12-01 04:58:14 +0530

    updated index.html

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ var  btntranslate = document.querySelector("#button");
 var  txtInput = document.querySelector("#txt-area")
 var outputDiv = document.querySelector(".output")
 
- var serverUrl ="https://api.funtranslations.com/translate/minion.json" 
+  var serverUrl ="https://api.funtranslations.com/translate/minion.json" 
 
-/** var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" */
+/** var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" **/
 
 function getTransUrl(input){
-    var url =  serverUrl + "?text"+ input 
+    var url =  serverUrl + "?"+"text"+ input 
     return url
 
 }
@@ -25,7 +25,8 @@ function clickEventHandler() {
         .then(response => response.json())
         .then(json =>{
             var output = json.contents.translated
-            outputDiv.innerText= output})
+            outputDiv.innerText= output
+        })
         .catch(errorHandler)
     
 }
